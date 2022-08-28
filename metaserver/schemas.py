6a7c2791bb9e06c6f37721532a3d4f27bcc81be7
stfwn <stefan@stfwn.com>
2022-08-28 10:28:13 +0200

    Allow letter colors in clan names (close #20)

--- metaserver/schemas.py
+++ metaserver/schemas.py
@@ -82,8 +82,8 @@ class ClanCreate(BaseModel):
             "^", ""
         ).isalnum(), "Only ascii letters, numbers and '^' allowed"
         assert re.match(
-            r"^(?!.*\^(?![\d]{3})).*$", v
-        ), "'^' must always be followed by exactly three numbers"
+            r"^(?!.*\^(?!([\d]{3}|[rgbwkycm]))).*$", v
+        ), "'^' must always be followed by exactly three numbers or one of 'rgbwkycm'"
         assert v.count("^") <= max_colors, "Clan tags can contain at most 4 colors"
         assert (
             len(re.sub(r"\^[\d]{3}", "", v)) <= max_letters
